ec0d88ce249c149d35886a55794415dbe739f58b
darcy <darcelisgutierrez@gmail.com>
2026-07-15 17:03:07 -0400

    //it brokennnn - need to figure

--- Assets/Scripts/DragUI.cs
+++ Assets/Scripts/DragUI.cs
@@ -15,16 +15,21 @@ public class DragUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
     public GameObject butterfly;
     public GameObject butterflyBtn;
     public Transform handPanel;
+    public Transform accessoryPanel;
     Camera cam;
+    public Vector2 startingPos;
+
      public void Start()
      {
+        startingPos = new Vector2 (GetComponent<Transform>().position.x, GetComponent<Transform>().position.y);
+        Debug.Log(startingPos);
           cam = Camera.main;
           if (!spriteOptions.defaultSprite)
           {
                Debug.LogError(gameObject.name + " does not have a valid default sprite");
           }
           
-          spriteImage.sprite = spriteOptions.defaultSprite;
+         spriteImage.sprite = spriteOptions.defaultSprite;
      }
 
     // Start is called before the first frame update
@@ -33,38 +38,56 @@ public class DragUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
         Debug.Log("Begin Dragging");
         if (!spriteOptions.heldSprite)
         {
-               Debug.LogError(gameObject.name + " does not have a valid held sprite");
-          }
+            Debug.LogError(gameObject.name + " does not have a valid held sprite");
+        }
         spriteImage.sprite = spriteOptions.heldSprite;
         parentAfterDrag = transform.parent; // if the item is in a panel or grid to be placed, then make sure to unparent
         transform.SetParent(transform.root);
         transform.SetAsLastSibling(); // place on top of all of the other UI elements so it doesnt clip through
    }
-   public void OnDrag(PointerEventData eventData)
-   {    
+
+    public void OnDrag(PointerEventData eventData)
+       {    
         Debug.Log("Dragging");
         Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0;
         transform.position = mousePosition; // while we're dragging, make sure to follow the mouse on the screen
 
-   }
-   public void OnEndDrag(PointerEventData eventData)
-   {
+       }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
      Debug.Log("END Dragging");
      transform.SetParent(handPanel); // reparent the item to the panel/grid its on 
-     if(returnToDefault == true)
-     {
-           //add a bool for "change after drag and for specific accessories make sure to set which ones chanage and which dont 
-        spriteImage.sprite = spriteOptions.defaultSprite;  
-        transform.SetParent(handPanel); 
-     }
-        
-        
+        //if the accessory was placed within bounds, put it in the hand panel
+        // else if the accessory is not within bounds, reparent it back, and snap it to the original position
 
+
+        //bool for "change after drag and for specific accessories make sure to set which ones chanage and which dont 
+        if (returnToDefault == true)
+         {
+            spriteImage.sprite = spriteOptions.defaultSprite;  
+            transform.SetParent(handPanel);
+         }
    }
-   public void ButterflyFlyAway()
+ public void OnCollisionStay2D(Collision2D collision) // as long as the accessory is in bounds, do this
+    {
+
+        Debug.Log("I AM CONNECTING Stay");
+        transform.SetParent(handPanel);
+    }
+    public void OnCollisionExit2D(Collision2D exit) //when the accessory leaves bounds, do this
+    {
+        Debug.Log("iM NO LONGER CONNECTED, exit");
+        transform.SetParent(accessoryPanel);
+    }
+
+    public void ButterflyFlyAway()
    {
      butterfly.SetActive(true); //just show the butterfly dog 
      butterflyBtn.SetActive(false);
    }
+   
+    
+
 }
